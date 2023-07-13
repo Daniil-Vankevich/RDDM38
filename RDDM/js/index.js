f5d2a0bd9e038b26debe28adb551edc04d9ca024
Daniil-Vankevich <danya.vankevich@mail.ru>
2023-07-13 23:59:42 +0300

    header-fixed js

--- RDDM/js/index.js
+++ RDDM/js/index.js
@@ -4,3 +4,15 @@ const menuBtn = document.querySelector('.menu__btn');
 menuBtn.addEventListener('click', () => {
 	headerNav.classList.toggle('header__nav-active')
 });
+
+
+// header fixed...........
+
+window.onscroll = function showHeader() {
+	let header = document.querySelector('.header');
+	if (window.scrollY >10) {
+		header.classList.add('header__fixed');
+	} else {
+		header.classList.remove('header__fixed')
+	}
+}
