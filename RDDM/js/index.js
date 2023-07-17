e986e7a82afb00b35f3dfee70be45bcd21f8e892
Daniil-Vankevich <danya.vankevich@mail.ru>
2023-07-17 21:39:50 +0300

    header animation done

--- RDDM/js/index.js
+++ RDDM/js/index.js
@@ -10,9 +10,30 @@ menuBtn.addEventListener('click', () => {
 
 window.onscroll = function showHeader() {
 	let header = document.querySelector('.header');
-	if (window.scrollY >0) {
+	if (window.scrollY > 0) {
 		header.classList.add('header__fixed');
 	} else {
 		header.classList.remove('header__fixed')
 	}
 }
+
+// header fixed padding...........
+
+
+let m = document.querySelector(".main");
+let h = document.querySelector(".header");
+let hHeight;
+
+function setTopPadding() {
+  hHeight = h.offsetHeight;
+  m.style.paddingTop = hHeight + "px";
+}
+
+
+window.onload = function() {
+  setTopPadding();
+};
+
+window.onresize = function() {
+  setTopPadding();
+};
